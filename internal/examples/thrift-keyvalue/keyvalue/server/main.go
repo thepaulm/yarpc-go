a91625bea49faf32ede6c52227a6bd82d67d500f
François-Xavier Bourlet <bombela@uber.com>
2017-05-22 18:00:16 -0700

    many test itself

--- internal/examples/thrift-keyvalue/keyvalue/server/main.go
+++ internal/examples/thrift-keyvalue/keyvalue/server/main.go
@@ -22,6 +22,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -33,6 +34,9 @@ import (
 	"go.uber.org/yarpc/api/transport"
 	"go.uber.org/yarpc/internal/examples/thrift-keyvalue/keyvalue/kv"
 	"go.uber.org/yarpc/internal/examples/thrift-keyvalue/keyvalue/kv/keyvalueserver"
+	"go.uber.org/yarpc/internal/examples/thrift-keyvalue/keyvalue/many/services"
+	kvsrv "go.uber.org/yarpc/internal/examples/thrift-keyvalue/keyvalue/many/services/keyvalueserver"
+	"go.uber.org/yarpc/internal/examples/thrift-keyvalue/keyvalue/many/unions"
 	"go.uber.org/yarpc/x/yarpcmeta"
 
 	"go.uber.org/yarpc"
@@ -118,6 +122,9 @@ func do() error {
 		Inbounds: yarpc.Inbounds{inbound},
 	})
 
+	h := kvh{}
+	dispatcher.Register(kvsrv.New(&h))
+
 	handler := handler{items: make(map[string]string)}
 	dispatcher.Register(keyvalueserver.New(&handler))
 
@@ -127,5 +134,41 @@ func do() error {
 		return err
 	}
 
+	i := dispatcher.Introspect()
+	PrettyPrint(i)
+	PrettyPrint(i.Procedures.IDLTree())
+
 	select {} // block forever
+	return nil
+}
+
+func PrettyPrint(v interface{}) {
+	b, _ := json.MarshalIndent(v, "", "  ")
+	println(string(b))
+}
+
+type kvh struct{}
+
+func (h *kvh) DeleteValue(ctx context.Context, Key *services.Key) error {
+	panic("not implemented")
+}
+
+func (h *kvh) GetManyValues(ctx context.Context, Range []services.Key) ([]*unions.ArbitraryValue, error) {
+	panic("not implemented")
+}
+
+func (h *kvh) GetValue(ctx context.Context, Key *services.Key) (*unions.ArbitraryValue, error) {
+	panic("not implemented")
+}
+
+func (h *kvh) SetValue(ctx context.Context, Key *services.Key, Value *unions.ArbitraryValue) error {
+	panic("not implemented")
+}
+
+func (h *kvh) SetValueV2(ctx context.Context, Key services.Key, Value *unions.ArbitraryValue) error {
+	panic("not implemented")
+}
+
+func (h *kvh) Size(ctx context.Context) (int64, error) {
+	panic("not implemented")
 }
